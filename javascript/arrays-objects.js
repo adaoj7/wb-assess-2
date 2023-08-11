4941781eb0c06918ec960d7ffcbd080b524a15b9
Adam Johnson <adaojohnson902@gmail.com>
2023-08-11 12:09:47 -0600

    first section

--- javascript/arrays-objects.js
+++ javascript/arrays-objects.js
@@ -5,38 +5,46 @@
 // - the boolean value true
 function myArr() {
   // Replace this with your code
-  let newArr = [4,'abc',['apple','berry','cherry'],true]
-  return newArr
+  let newArr = [4, "abc", ["apple", "berry", "cherry"], true];
+  return newArr;
 }
 
 // The following array, `nestedLetters`, contains many levels of nested arrays.
 // Return the letter 'z' from `nestedLetters` using bracket notation.
 function getZ() {
-  const nestedLetters = ['m', 'g', 'e', 'q', 'h', ['n', 'b', ['v', 'z', 'y', 'r']], 'a'];
-  return nestedLetters[5][2][1]
+  const nestedLetters = [
+    "m",
+    "g",
+    "e",
+    "q",
+    "h",
+    ["n", "b", ["v", "z", "y", "r"]],
+    "a",
+  ];
+  return nestedLetters[5][2][1];
   // Replace this with your code
 }
 
 // Use the spread operator to combine the following arrays into one array and return it.
 function flatAnimals() {
-  const forest = ['deer', 'bear', 'squirrel'];
-  const ocean = ['whale', 'shark', 'dolphin', 'octopus', 'starfish'];
-  const savannah = ['lion', 'zebra', 'lion', 'giraffe'];
-  const desert = ['rattlesnake', 'coyote'];
+  const forest = ["deer", "bear", "squirrel"];
+  const ocean = ["whale", "shark", "dolphin", "octopus", "starfish"];
+  const savannah = ["lion", "zebra", "lion", "giraffe"];
+  const desert = ["rattlesnake", "coyote"];
 
   // Replace this with your code
-  let habitats =[...forest, ...ocean, ...savannah, ...desert]
-  return habitats
+  let habitats = [...forest, ...ocean, ...savannah, ...desert];
+  return habitats;
 }
 
 // Now use the spread operator to return a copy of the animals array with 'elephant' at
 // the end.
 function animalsWithElephant() {
-  const animals = ['aardvark', 'bear', 'cat'];
+  const animals = ["aardvark", "bear", "cat"];
 
   // Replace this with your code
-  let animalsEle = [...animals, 'elephant']
-  return animalsEle
+  let animalsEle = [...animals, "elephant"];
+  return animalsEle;
 }
 // console.log(animalsWithElephant())
 
@@ -44,8 +52,8 @@ function animalsWithElephant() {
 // delete the rating property off of the gameInfo object.
 function improveGameRating() {
   const gameInfo = {
-    name: 'Splendor',
-    desc: 'Renaissance merchants race to grab gems, acquire property, and please nobility.',
+    name: "Splendor",
+    desc: "Renaissance merchants race to grab gems, acquire property, and please nobility.",
     players: [2, 3, 4],
     playingTime: 30,
     minAge: 10,
@@ -53,7 +61,7 @@ function improveGameRating() {
   };
 
   // Replace this with your code
-  delete gameInfo.rating
+  delete gameInfo.rating;
   return gameInfo;
 }
 
@@ -72,10 +80,9 @@ function evenShapes() {
 
   // Replace this with your code
   // for (i = )
-  for (const prop in shapes){
-    if(shapes[prop] % 2 !== 0){ 
-      delete shapes[prop]
-      
+  for (const prop in shapes) {
+    if (shapes[prop] % 2 !== 0) {
+      delete shapes[prop];
     }
   }
   // console.log(shapes)
@@ -90,25 +97,25 @@ function evenShapes() {
 function betterCourses() {
   const courses = [
     {
-      title: 'JavaScript 101',
-      instructor: 'Emily',
-      days: ['M', 'W', 'F'],
+      title: "JavaScript 101",
+      instructor: "Emily",
+      days: ["M", "W", "F"],
       time: 11,
       inPerson: false,
       homework: true,
     },
     {
-      title: 'UI Design',
-      instructor: 'Daniel',
-      days: ['T', 'Th'],
+      title: "UI Design",
+      instructor: "Daniel",
+      days: ["T", "Th"],
       time: 9,
       inPerson: true,
       homework: false,
     },
     {
-      title: 'Creating Servers',
-      instructor: 'Jess',
-      days: ['M', 'W'],
+      title: "Creating Servers",
+      instructor: "Jess",
+      days: ["M", "W"],
       time: 1,
       inPerson: true,
       homework: true,
@@ -116,13 +123,13 @@ function betterCourses() {
   ];
 
   // Replace this with your code
-    courses.forEach(ele => {
-      for (const prop in ele){
-        if (ele[prop] === true){
-          ele[prop] = false
-        }
+  courses.forEach((ele) => {
+    for (const prop in ele) {
+      if (ele[prop] === true) {
+        ele[prop] = false;
       }
-    })
+    }
+  });
   // for (let i = 0; i < courses.length; i++){
   //   for (let prop in obj){
   //     if (obj[prop] === true) {
@@ -133,38 +140,45 @@ function betterCourses() {
   // console.log(courses)
   return courses;
 }
-console.log(betterCourses())
+// console.log(betterCourses())
 // Use nested for loops to compare the letters in the lettersToPair array below.
 // When you find a pair, push the indexes of the letters into the pairsArray as an array.
 // For example, looping the array ['b', 'x', 'x', 'b']
 // should create the array [[0,3], [1,2]].
 function findPairs() {
-  const lettersToPair = ['e', 'k', 's', 'a', 'e', 's', 'a', 'n', 'k', 'n'];
+  const lettersToPair = ["e", "k", "s", "a", "e", "s", "a", "n", "k", "n"];
   const pairsArray = [];
 
   // Replace this with your code
-  lettersToPair.forEach(ele =>{
-    for (i = 0; i < lettersToPair.length; )
-      if (ele === i){
-        pairsArray.push([ele,i])
+  lettersToPair.forEach((ele, index) => {
+    //  pairsArray = lettersToPair.filter(eleComp => {
+    //   if(ele === eleComp){
+
+    for (let i = 0; i < lettersToPair.length; i++)
+      if (ele === lettersToPair[i]) {
+        if (i !== index) {
+          pairsArray.push([index, i], i);
+        }
       }
-  })
+  });
+  // console.log(pairsArray)
   return pairsArray;
 }
-console.log(findPairs())
+// console.log(findPairs())
+
 // For these problems we will be using the objects below, contactInfo and shippingInfo,
 // as well as objects that you will make based off these two.
 const contactInfo = {
-  name: 'Helen',
+  name: "Helen",
   phoneNumber: 1234445555,
-  email: 'helen@mymail.com',
+  email: "helen@mymail.com",
 };
 
 const shippingInfo = {
-  name: 'Helen',
-  street: '100 E. Main Street',
-  city: 'Anytown',
-  state: 'AZ',
+  name: "Helen",
+  street: "100 E. Main Street",
+  city: "Anytown",
+  state: "AZ",
   zipCode: 85004,
 };
 
@@ -172,18 +186,28 @@ const shippingInfo = {
 // shippingInfo objects and return it.
 function getHelensInfo() {
   // Replace this with your code
+  let getHelen = Object.assign(contactInfo, shippingInfo);
+  return getHelen;
 }
 
 // Return Helen's email using destructuring.
 function getHelensEmail() {
   // Replace this with your code
+  const { email } = contactInfo;
+  return email;
 }
 
 // Use destructuring to save the zip code and state
 // from shippingInfo to new variables and return [zipCode, state]
 function getZipAndState() {
   // Replace this with your code
+  let newVar = [];
+  const { state, zipCode } = shippingInfo;
+  newVar = [zipCode, state];
+  console.log(newVar);
+  return newVar;
 }
+console.log(getZipAndState());
 
 // Helen has a daughter named Ellen that lives at the same address.
 // Return an object with Helen's info in it using the spread operator on the
@@ -192,50 +216,54 @@ function getZipAndState() {
 // Return the new object.
 function getEllensInfo() {
   // Replace this with your code
+  let ellensInfo = { ...contactInfo, ...shippingInfo };
+  ellensInfo.name = "Ellen";
+  ellensInfo.email = "ellen@email.com";
+  return ellensInfo;
 }
 
 // Use the userInfo object below to complete problems 9-11.
 const userInfo = {
-  name: 'gn@rly_c0der_007',
-  password: 'reallySuperSecret1',
+  name: "gn@rly_c0der_007",
+  password: "reallySuperSecret1",
   settings: {
-    theme: 'dark',
+    theme: "dark",
     fontSize: 14,
     alerts: false,
   },
-  topics: ['food', 'hiking', 'gaming', 'tech'],
+  topics: ["food", "hiking", "gaming", "tech"],
   comments: [
     {
-      post: 'New Phones Coming in September',
-      comment: 'Excited to get one of these!',
+      post: "New Phones Coming in September",
+      comment: "Excited to get one of these!",
       upvotes: 5,
       responses: [
         {
           userId: 3827,
-          response: 'Me too!',
+          response: "Me too!",
         },
         {
           userId: 1040,
-          response: 'Want to learn about the pyramid scheme I bought into?',
+          response: "Want to learn about the pyramid scheme I bought into?",
         },
       ],
     },
     {
-      post: 'Best Hiking in Your Town',
-      comment: 'Trail #402 is closed in the winter',
+      post: "Best Hiking in Your Town",
+      comment: "Trail #402 is closed in the winter",
       upvotes: 100,
       responses: [
         {
           userId: 1084,
-          response: 'Thanks for the info',
+          response: "Thanks for the info",
         },
         {
           userId: 5498,
-          response: 'You saved me a trip out there, thank you!',
+          response: "You saved me a trip out there, thank you!",
         },
         {
           userId: 3597,
-          response: 'Good to know',
+          response: "Good to know",
         },
       ],
     },
@@ -245,18 +273,21 @@ const userInfo = {
 // Return the value of alerts in gn@rly_c0der_007's settings using dot notation.
 function shouldAlert() {
   // Replace this with your code
+  return userInfo.settings.alerts;
 }
 
 // Return the last item in gn@rly_c0der_007's topics array
 // using dot and/or bracket notation.
 function lastTopic() {
   // Replace this with your code
+  return userInfo.topics[userInfo.topics.length-1];
 }
 
 // Return the userId of the first response to
 // gn@rly_c0der_007's 2nd comment using dot/bracket notation.
 function firstResponseId() {
   // Replace this with your code
+  return userInfo.comments[1].responses[0].userId;
 }
 
 export {
